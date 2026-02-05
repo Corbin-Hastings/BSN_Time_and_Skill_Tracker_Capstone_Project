afe5838534d8e68f3e437800c16a6129f07d3d30
Corbin Hastings <Corbinday@gmail.com>
2026-02-05 16:55:45 -0700

    working on the database

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -2,14 +2,43 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.db.models import Q
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+class Level(models.TextChoices):
+    sim = 's','S'
+    assisted = 'a','A'
+    independent = 'i','I'
+class AccountType(models.TextChoices):
+    STUDENT = 'STU'
+    INSTRUCTOR = 'INST'
+    ADMIN = 'ADMIN'
 
+class User(models.Model):
+    name= models.CharField(max_length=64)
+    email= models.CharField(max_length=128)
+    admin= models.BooleanField(default=False)
+    roll= models.CharField(max_length=5,default='STU',choices=AccountType.choices)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Skill(models.Model):
+    skill = models.CharField(max_length=128)# TODO: make this a text choice. this means a new choice class
+
+class StudentSkill(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    skill =models.ForeignKey(Skill, on_delete=models.CASCADE)
+    level = models.CharField(max_length=1,choices=Level.choices)
+    date_learned = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('student','skill')
+class Skill(models.Model):
+
+    skill = models.CharField(max_length=300)
+    level = models.CharField(choices=Level.choices, max_length=2)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(level__in=Level.values),
+                name='level_val'
+            )
+        ]
