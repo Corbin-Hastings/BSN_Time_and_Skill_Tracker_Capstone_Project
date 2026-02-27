7d345d6d6e57a9bbf4d3524ba9102ee5056c3160
izzy-co <isabel.co293@gmail.com>
2026-02-27 14:00:39 -0700

    haha

--- BSNTracker/accounts/forms.py
+++ BSNTracker/accounts/forms.py
@@ -2,7 +2,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+
+class CustomUserCreationForm(UserCreationForm): #this is stupid, you are stupid Corbin
     class Meta:
         model = CustomUser
         fields = ('email','first_name','last_name')
