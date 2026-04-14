f8b0d8b6897fba18ee0c0d9b5273095ea83b066c
Corbin Hastings <Corbinday@gmail.com>
2026-04-14 16:40:10 -0600

    added time zone, added auto staff->inst, ensure static works in PA, rebuilt DB

--- BSNTracker/accounts/admin.py
+++ BSNTracker/accounts/admin.py
@@ -3,9 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from .models import *
+from instructor.models import Instructor
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['first_name','last_name','email','date_joined','is_staff','is_superuser','is_active']
     list_editable = ['is_staff','is_superuser','is_active']
     search_fields = ['first_name','last_name','email']
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if obj.is_staff:
+            Instructor.objects.create(user_id=obj)
+
+        else:
+            Instructor.objects.filter(user_id=obj).delete()
+
--- BSNTracker/instructor/models.py
+++ BSNTracker/instructor/models.py
@@ -10,7 +10,7 @@ class Course(models.Model):
 class Instructor(models.Model):
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
     location = models.CharField(max_length=150)
-    courses = models.ManyToManyField(Course)
+    courses = models.ManyToManyField(Course, blank=True)
 
     def __str__(self):
         return f"{self.user_id.first_name} {self.user_id.last_name}"
