16aa8aceb2a7a18fcc058742129b4b5c471be0fb
Corbin Hastings <Corbinday@gmail.com>
2026-04-14 15:53:18 -0600

    changing the admin page

--- BSNTracker/accounts/admin.py
+++ BSNTracker/accounts/admin.py
@@ -6,6 +6,6 @@ from .models import *
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['first_name','last_name','email','date_joined','is_staff','is_active']
-    list_editable = ['is_staff','is_active']
+    list_display = ['first_name','last_name','email','date_joined','is_staff','is_superuser','is_active']
+    list_editable = ['is_staff','is_superuser','is_active']
     search_fields = ['first_name','last_name','email']
--- BSNTracker/skills/admin.py
+++ BSNTracker/skills/admin.py
@@ -10,3 +10,9 @@ from .models import *
 class AdminSkillsLog(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
+
+@admin.register(StudentSkill)
+class AdminStudentSkill(admin.ModelAdmin):
+    list_display = ['student','skill','level','approved','instructor']
+    list_editable = ['approved','level']
+    search_fields = ['name']
